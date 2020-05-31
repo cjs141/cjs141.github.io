272d82dd597c0374af1c5050c4914fbd106e8f8f
cjs141 <cjs141@txstate.edu>
2020-05-31 08:01:30 -0500

    Add build dir

---
+++ build/precache-manifest.72ae24dadd477af14b668d2f42f77b5b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4b054c97422cb43ddeaa32c8d202dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b589026bf1038fba0bc",
+    "url": "/static/css/2.def00553.chunk.css"
+  },
+  {
+    "revision": "ada637408c37b24cc99f",
+    "url": "/static/css/main.264ad9b4.chunk.css"
+  },
+  {
+    "revision": "0b589026bf1038fba0bc",
+    "url": "/static/js/2.165e24d4.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.165e24d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada637408c37b24cc99f",
+    "url": "/static/js/main.2975b7f3.chunk.js"
+  },
+  {
+    "revision": "99a1df5724a42cc4fef3",
+    "url": "/static/js/runtime-main.4a3acf61.js"
+  },
+  {
+    "revision": "39ad5cbd60ab71b5de781cd18085793f",
+    "url": "/static/media/BusinessPic2.39ad5cbd.jpg"
+  },
+  {
+    "revision": "94f661aa00d2cd605dd40a849a2abfd9",
+    "url": "/static/media/BusinessPic3.94f661aa.jpg"
+  },
+  {
+    "revision": "987f91efdbf2548c9c93e825b92e843c",
+    "url": "/static/media/ResumeCopy.987f91ef.pdf"
+  }
+]);
